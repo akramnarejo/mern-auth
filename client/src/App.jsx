73f0ef7a1c5a3fce5428faa51a777add556fb1c3
Muhammad Akram <ak.narejo@gmail.com>
2024-03-02 18:51:39 +0500

    created client section

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div className="p-4">
+        <h1 className="text-8xl text-red-800">hello world</h1>
+        <p className="text-xl text-gray-700">
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+        </div>
+    </>
+  )
+}
+
+export default App
