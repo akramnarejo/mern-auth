0245a2b16195f59ba442ef48f313fe893e41be4b
Muhammad Akram <ak.narejo@gmail.com>
2024-03-03 17:11:56 +0500

    add header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,27 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Home from './pages/Home';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
-import Profile from './pages/Profile';
-import About from './pages/About';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+import Header from "./components/Header";
 function App() {
-
   return (
     <>
       <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home  />}/>
-          <Route path='/sign-in' element={<SignIn />} />
-          <Route path='/sign-up' element={<SignUp />} />
-          <Route path='/profile' element={<Profile />} />
-          <Route path='/about' element={<About />} />
-        </Routes>
+        <Header />
+        <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/sign-in" element={<SignIn />} />
+            <Route path="/sign-up" element={<SignUp />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
