a381fdd14ad6dfb3a9c5221fcb2fcca64b9ab54f
Muhammad Akram <ak.narejo@gmail.com>
2024-03-04 13:22:34 +0500

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,7 +1,12 @@
 import express from 'express'
-
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 const app = express()
+dotenv.config()
 
+mongoose.connect(process.env.MONGO_URL).then(() => console.log('mongodb connected!')).catch(err => {
+    console.log('mongodb failed to connect. ', err)
+})
 app.listen(3000, () => {
     console.log('server listening on port 3000')
 })
